609d300611f442012f81141f78636a87c6c1fa51
koding-with-kelly <kelly.klam.lam@gmail.com>
2021-03-18 21:14:28 -0700

    php

--- images/mail.php
+++ images/mail.php
@@ -6,11 +6,13 @@ $email= $_POST['email'];
 $message= $_POST['message'];
 $to = "kelly.kn.lam@gmail.com";
 $subject = "Mail From portfolio";
+
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = "From:" .$email;
+
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
 //redirect
-header("Location:thankyou.html");
+header("Location: index.html");
 ?>
