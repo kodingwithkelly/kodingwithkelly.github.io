be598d5773d7e5ab408945f7382c9dee774f6513
koding-with-kelly <kelly.klam.lam@gmail.com>
2021-03-21 03:36:41 -0700

    slideshow

---
+++ assets/js/slideshow.js
@@ -0,0 +1,29 @@
+var slideIndex = []; // This will be automatically populated
+var slideId = ["slides1", "slides2", "slides3", "slides4"]
+initialiseSlides();
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2); // Note that this is set to 1 now
+showSlides(1, 3);
+
+// Initalise all starting slide indexes
+function initialiseSlides(){
+    for (var i = 0; i < slideId.length; i++){
+       slideIndex[i] = 1;
+    }
+ }
+
+function plusDivs(n, no) {
+  showDivs(slideIndex[no] += n, no);
+}
+
+function showDivs(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block"; 
+}
